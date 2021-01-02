dcb0f0f56800aa42f4b4cf904d1dfa5550ae8f7a
satyamsingh <singhsatyamsahab1999@gmail.com>
2021-01-02 14:13:26 +0530

    initial commit

---
+++ in.co.rays/src/main/java/project3/in/co/rays/App.java
@@ -0,0 +1,13 @@
+package project3.in.co.rays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
